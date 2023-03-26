57078ee8744ec33854e292d29034ebe33a97693a
Yestay Tastanov <yestay@Yestays-MacBook-Pro.local>
2023-03-26 23:32:44 +0600

    dot separated list tile component added

---
+++ lib/mobile/components/dot_separated_list_tile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DotSeparatedListTile extends StatelessWidget {
+  final String label;
+  final String value;
+  final TextStyle? style;
+
+  const DotSeparatedListTile({
+    super.key,
+    required this.label,
+    required this.value,
+    this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          label,
+          style: style,
+        ),
+        const SizedBox(width: 5),
+        Expanded(
+          child: Text(
+            '.' * 100,
+            overflow: TextOverflow.fade,
+            softWrap: false,
+          ),
+        ),
+        const SizedBox(width: 5),
+        Text(
+          value,
+          style: style,
+        ),
+      ],
+    );
+  }
+}
--- lib/mobile/pages/expense_list/view/expense_list_screen.dart
+++ lib/mobile/pages/expense_list/view/expense_list_screen.dart
@@ -4,6 +4,8 @@ import 'package:bill_share/mobile/components/wavy_container/wavy_container.dart'
 import 'package:bill_share/mobile/pages/expense_list/view/expense_list_cubit.dart';
 import 'package:bill_share/mobile/pages/expense_list/view/expense_list_state.dart';
 import 'package:bill_share/services/navigation/api/navigation_provider.dart';
+import 'package:bill_share/styles/colors.dart';
+import 'package:bill_share/styles/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseListScreen
@@ -24,6 +26,7 @@ class ExpenseListScreen
   @override
   Widget buildPage(context, cubit, state) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
         title: Text('Expenses'),
       ),
@@ -32,18 +35,47 @@ class ExpenseListScreen
         itemBuilder: (context, index) {
           return WavyContainer(
             container: Container(
-                padding: EdgeInsets.all(8),
+                color: AppColors.white,
+                padding: EdgeInsets.all(20),
                 child: Column(
                   children: [
                     Row(
                       children: [
                         Icon(Icons.circle),
-                        Column(
-                          children: [
-                            Text(state.details[index].name),
-                            Text('${state.details[index].totalPrice}'),
-                            Text('${state.details[index].paidPrice}'),
-                          ],
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                state.details[index].name,
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: FontSizes.h3,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text('Total'),
+                                  const SizedBox(width: 5),
+                                  Expanded(
+                                    child: Text(
+                                      '.' * 100,
+                                      overflow: TextOverflow.fade,
+                                      softWrap: false,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text('${state.details[index].totalPrice}'),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text('${state.details[index].paidPrice}'),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ],
                     ),
